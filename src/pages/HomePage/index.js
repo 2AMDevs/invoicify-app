601ef4b5c1383d9fb702dbd48b46ead7420ebefe
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-07-04 08:25:37 +0530

    teaching tooltip

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,25 +1,8 @@
 import React from 'react'
 
-import { DefaultButton, ActionButton } from 'office-ui-fabric-react'
-
 import { Header, SideBar } from '../../components'
 
 import './index.scss'
-
-const menuProps = {
-  items: [
-    {
-      key: 'emailMessage',
-      text: 'Email message',
-      iconProps: { iconName: 'Mail' },
-    },
-    {
-      key: 'calendarEvent',
-      text: 'Calendar event',
-      iconProps: { iconName: 'Calendar' },
-    },
-  ],
-}
 
 const HomePage = () => (
   <div className="home-page">
@@ -28,19 +11,6 @@ const HomePage = () => (
     </header>
     <mail className="home-page__content">
       <SideBar className="home-page__content__sidebar" />
-      <DefaultButton
-        text="Primary"
-        primary
-        split
-        splitButtonAriaLabel="See 2 options"
-        aria-roledescription="split button"
-        menuProps={menuProps}
-        onClick={() => {
-          alert('clicked')
-        }}
-        disabled={false}
-        checked={false}
-      />
     </mail>
   </div>
 )
