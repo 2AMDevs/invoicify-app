b0ced1cc93a252c9288f885e55c50131f0d0aeff
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-07-05 12:55:30 +0530

    Force dark-mode irrespective of system preferences

--- public/electron.js
+++ public/electron.js
@@ -1,49 +1,50 @@
-const { app, BrowserWindow } = require("electron");
-const isDev = require("electron-is-dev");
-const path = require("path");
+const { app, BrowserWindow, systemPreferences } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
 
-function createWindow() {
+function createWindow () {
   // Create the browser window.
+  systemPreferences.setAppLevelAppearance('dark')
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
-  });
+  })
 
   // and load the index.html of the app.
   win.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 
   // Open the DevTools.
-  if (isDev) win.webContents.openDevTools();
+  if (isDev) win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
