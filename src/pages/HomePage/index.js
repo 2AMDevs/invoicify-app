722cdea2c294497cbba0c3cac0ffc8a9a46856b3
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-07-05 20:16:53 +0530

    feat: Add Invoice Basic Form

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,14 +1,72 @@
+import { Stack } from 'office-ui-fabric-react/lib/Stack'
+import { MaskedTextField, TextField } from 'office-ui-fabric-react/lib/TextField'
+import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox'
+import { DatePicker } from 'office-ui-fabric-react'
+
 import React from 'react'
 import './index.scss'
 
+const stackTokens = { childrenGap: 15 }
+const stackStyles = { root: { width: 650 } }
+const columnProps = {
+  tokens: { childrenGap: 100 },
+  styles: { root: { width: 650 } },
+}
+
 const HomePage = () => (
   <div className="home-page">
-    <p className="home-page__content">
-      That&apos;s here. That&apos;s home. That&apos;s us.
-      On it everyone you love, everyone you know, everyone you ever heard of,
-      every human being who ever was, lived out their lives.
-    </p>
+    <Stack
+      vertical
+      tokens={stackTokens}
+      styles={stackStyles}
+    >
+      <Stack
+        horizontal
+        {...columnProps}
+      >
+        <TextField
+          label="Invoice No."
+          disabled
+          defaultValue="D001"
+        />
+        <DatePicker
+          isRequired
+          value={new Date()}
+          label="Date"
+          placeholder="Select a date..."
+          ariaLabel="Select a date"
+        />
+      </Stack>
+      <TextField
+        required
+        label="Customer Name"
+      />
+      <Stack
+        horizontal
+        {...columnProps}
+      >
+        <MaskedTextField
+          label="Party GSTIN"
+          mask="99-**********-*Z*"
+        />
+        <MaskedTextField
+          label="Mobile Number"
+          mask="+\91 9999999999"
+        />
+        <Checkbox
+          label="Same State (Raj.)"
+          // onChange={_onChange}
+        />
+      </Stack>
+      <Stack {...columnProps}>
+        <TextField
+          label="Party Address"
+          placeholder="Nimbahera"
+        />
+      </Stack>
+    </Stack>
   </div>
+// TODO: npm install convert-rupees-into-words
 )
 
 export default HomePage
