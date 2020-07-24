20793d295a2645a3835e663a8aa3f385146ea14d
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-07-24 19:21:31 +0530

    new

--- public/printPdf.js
+++ public/printPdf.js
@@ -1,20 +1,28 @@
 const fs = require('fs')
 const path = require('path')
 
-const {
-  BrowserWindow,
-} = require('electron')
+const PDFWindow = require('electron-pdf-window')
+
+// const {
+//   BrowserWindow,
+// } = require('electron')
 
 const print = (pdfBytes) => {
-  const win = new BrowserWindow({ show: true, plugins: true })
+  // const win = new BrowserWindow({ show: true, plugins: true })
   fs.writeFile(path.join(__dirname, 'print.pdf'), pdfBytes, () => {})
-  win.loadURL(`file://${__dirname}/print.pdf`)
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.print({ silent: true })
-    setTimeout(() => {
-      win.close()
-    }, 1000)
+  const win = new PDFWindow({
+    width: 800,
+    height: 600,
   })
+
+  // win.loadURL('http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
+  win.loadURL(`file://${__dirname}/print.pdf`)
+  // win.webContents.on('did-finish-load', () => {
+  //   win.webContents.print({ silent: true })
+  //   setTimeout(() => {
+  //     win.close()
+  //   }, 1000)
+  // })
 }
 
 module.exports = {
