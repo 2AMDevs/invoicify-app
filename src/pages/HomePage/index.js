7face0d88775708ad87624b888a79a19a8fdd8d8
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-07-26 09:50:34 +0530

    remove old pdf preview

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,6 @@
 import './index.scss'
 import React, { useState } from 'react'
 
-import { Stack } from 'office-ui-fabric-react/lib/Stack'
 import { Panel } from 'office-ui-fabric-react/lib/Panel'
 import { useConstCallback } from '@uifabric/react-hooks'
 import { pdfjs, Document, Page } from 'react-pdf'
@@ -10,60 +9,50 @@ import { Invoice } from '../../components'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
-const deviceWidth = document.documentElement.clientWidth
-const columnProps = {
-  tokens: { childrenGap: deviceWidth * 0.07 },
-  styles: { root: { width: deviceWidth * 0.9 } },
-}
-
 const HomePage = () => {
   const [preview, setPreview] = useState('')
   const [isPreviewOpen, setIsPreviewOpen] = React.useState(false)
 
+  const openPanel = useConstCallback(() => setIsPreviewOpen(true))
+  const dismissPanel = useConstCallback(() => setIsPreviewOpen(false))
+
   const showPdfPreview = (pdfBytes) => {
     setPreview(pdfBytes)
-    setIsPreviewOpen(true)
+    openPanel(true)
   }
 
   return (
     <div
       className="home-page"
     >
-      <Stack
-        horizontal
-        {...columnProps}
+      <Invoice
+        showPdfPreview={showPdfPreview}
+      />
+      <Panel
+        isLightDismiss
+        className="home-page__panel"
+        isOpen={isPreviewOpen}
+        onDismiss={dismissPanel}
+        closeButtonAriaLabel="Close"
+        headerText="Invoice preview"
       >
-        <Invoice
-          showPdfPreview={showPdfPreview}
-        />
-        <Panel
-          isLightDismiss
-          className="home-page__panel"
-          isOpen={isPreviewOpen}
-          onDismiss={() => {
-            setIsPreviewOpen(false)
-          }}
-          closeButtonAriaLabel="Close"
-          headerText="Invoice preview"
-        >
-          {preview?.length
-            ? (
-              <Document
-                file={{ data: preview }}
-                className="home-page__panel__doc"
-              >
-                <Page
-                  pageNumber={1}
-                  scale={0.65}
-                />
-              </Document>
-            ) : (
-              <div className="preview-area">
-                <div>Invoice Preview</div>
-              </div>
-            )}
-        </Panel>
-      </Stack>
+        {preview?.length
+          ? (
+            <Document
+              file={{ data: preview }}
+              className="home-page__panel__doc"
+            >
+              <Page
+                pageNumber={1}
+                scale={0.65}
+              />
+            </Document>
+          ) : (
+            <div className="preview-area">
+              <div>Invoice Preview</div>
+            </div>
+          )}
+      </Panel>
     </div>
   )
 }
