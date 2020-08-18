e086eeed43ff12d26d2d9b40adfcf7d9ba1a1ff7
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-08-18 18:19:28 +0530

    :zap: feat: Update version before restart

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -356,12 +356,13 @@ const groupBy = (array, key) => array.reduce((result, currentValue) => {
   return result
 }, {})
 
-ipcRenderer.on('updateDownloaded', () => {
+ipcRenderer.on('updateDownloaded', (_event, info) => {
   const notification = document.getElementById('notification')
   const message = document.getElementById('message')
   const restartButton = document.getElementById('restart-button')
   if (message) message.innerText = UPDATE_RESTART_MSG
   restartButton.classList.remove('hidden')
+  localStorage.version = info.version
   notification.parentElement.parentElement.parentElement.classList.remove('hidden')
 })
 
