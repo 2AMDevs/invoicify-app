89109171afae326dd426fbfbafd6c0f26004fcbe
Mohit K. Yadav <mohitkyadav@outlook.com>
2020-08-18 12:25:53 +0530

    input length for gst

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -233,6 +233,7 @@ const defaultPrintSettings = [
     required: true,
     disabled: false,
     type: TEXT,
+    inputLength: 15,
     regex: '^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$',
     row: 3,
     size: defaultPageSettings.fontSize,
