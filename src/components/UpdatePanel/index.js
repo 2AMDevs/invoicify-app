331961d73c53c8d53c28bd6cd850d25c3d64f06b
Aashutosh Rathi <aashutoshrathi@gmail.com>
2021-02-20 17:28:40 +0530

    :sparkles: feat: Update Panel Layout changes

--- src/components/UpdatePanel/index.js
+++ src/components/UpdatePanel/index.js
@@ -1,55 +1,26 @@
 import 'github-markdown-css'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-import { Spinner, SpinnerSize } from 'office-ui-fabric-react'
-import ReactMarkdown from 'react-markdown'
+import { Icon, Separator, Stack } from 'office-ui-fabric-react'
+
+import DownloadSection from './DownloadSection'
 import './index.scss'
+import ReleaseNotes from './ReleaseNotes'
 
-const UpdatesPanel = ({ tag }) => {
-  const [notes, setNotes] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetch(`https://api.aashutosh.dev/invoicify-release/${tag}`)
-      const jsonData = await data.json()
-      // this is trick to get newline, yeah!
-      jsonData.body.replace(/\r/gi, '  ')
-      setNotes([jsonData])
-      setLoading(false)
-    }
-
-    getData()
-  }, [tag])
-
-  if (loading) {
-    return (
-      <div className="full-height">
-        <Spinner
-          size={SpinnerSize.large}
+const UpdatesPanel = ({ tag }) => (
+  <>
+    <DownloadSection />
+    <Stack>
+      <Separator className="separator-stack">
+        Release Notes
+        <Icon
+          iconName="ProductRelease"
+          className="separator-stack__icon"
         />
-      </div>
-    )
-  }
-
-  return (
-    <div className="release-notes">
-      <br />
-      <div className="release-notes__body">
-        {(notes?.map((release) => (
-          <div
-            key={release.node_id}
-            className="markdown-body"
-          >
-            <ReactMarkdown allowDangerousHtml>
-              {release.body}
-            </ReactMarkdown>
-            <br />
-          </div>
-        )))}
-      </div>
-    </div>
-  )
-}
+      </Separator>
+    </Stack>
+    <ReleaseNotes tag={tag} />
+  </>
+)
 
 export default UpdatesPanel
