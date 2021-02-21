9374573ce1a67278556ea948fd89d38884f292ac
Aashutosh Rathi <aashutoshrathi@gmail.com>
2021-02-21 12:59:07 +0530

    :bug: fix: handle error in release note API

--- src/components/UpdatePanel/ReleaseNotes/index.js
+++ src/components/UpdatePanel/ReleaseNotes/index.js
@@ -8,26 +8,39 @@ import '../index.scss'
 const ReleaseNotes = ({ tag }) => {
   const [notes, setNotes] = useState([])
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
   useEffect(() => {
     const getData = async () => {
-      const data = await fetch(`${process.env.RELEASE_NOTES_API}${tag}`)
-      const jsonData = await data.json()
-      // this is trick to get newline, yeah!
-      jsonData.body.replace(/\r/gi, '  ')
-      setNotes([jsonData])
-      setLoading(false)
+      try {
+        setError(false)
+        const data = await fetch(`${process.env.REACT_APP_RELEASE_NOTES_API}${tag}`)
+        const jsonData = await data.json()
+        // this is trick to get newline, yeah!
+        jsonData.body.replace(/\r/gi, '  ')
+        setNotes([jsonData])
+        setLoading(false)
+      } catch (e) {
+        setLoading(false)
+        setError(true)
+      }
     }
-
     getData()
   }, [tag])
 
   if (loading) {
     return (
-
       <div className="full-height">
         <Spinner
           size={SpinnerSize.large}
         />
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="full-height center-self">
+        <div>Unable to fetch release notes from servers!</div>
       </div>
     )
   }
