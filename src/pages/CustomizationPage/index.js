0b729e24b983e06a73995f891bffd460db72e76c
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-07-22 08:30:53 +0530

    init new page

---
+++ src/pages/CustomizationPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './index.scss'
+
+const CustomizationPage = () => (
+  <div className="customization-page">
+    <h1>Customization Page</h1>
+  </div>
+)
+
+export default CustomizationPage
