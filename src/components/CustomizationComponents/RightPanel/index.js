9e744ec75838f288822438da842e259a684cf315
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-07-30 20:18:29 +0530

    handle drag

--- src/components/CustomizationComponents/RightPanel/index.js
+++ src/components/CustomizationComponents/RightPanel/index.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import cn from 'classnames'
 
 import './index.scss'
 
-const RightPanel = ({ selectedItem }) => (
-  <div
-    className={cn('right-panel', {
-      'right-panel--active': selectedItem,
-    })}
-  >
-    <h1 className="right-panel__header">Set position of the Field on invoice</h1>
-    <div className="right-panel__preview" />
-  </div>
-)
+const RightPanel = ({ selectedItem }) => {
+  const [pos, setPos] = useState({ x: 0, y: 0 })
+  const [dragging, setDragging] = useState(false)
+
+  return (
+    <div
+      className={cn('right-panel', {
+        'right-panel--active': selectedItem,
+      })}
+    >
+      <h1 className="right-panel__header">Set position of the Field on invoice</h1>
+      <div
+        className="right-panel__preview"
+        onMouseMove={(e) => {
+          console.log(dragging)
+          if (!dragging) return
+
+          const rect = e.target.getBoundingClientRect()
+          setPos({ x: e.clientX - rect.left - 10, y: e.clientY - rect.top - 10 })
+        }}
+      >
+        <div
+          className="right-panel__preview__handle"
+          style={{ top: `${pos.y}px`, left: `${pos.x}px` }}
+          onMouseDown={() => setDragging(true)}
+          onMouseUp={() => setDragging(false)}
+          role="presentation"
+        />
+      </div>
+    </div>
+  )
+}
 
 export default RightPanel
