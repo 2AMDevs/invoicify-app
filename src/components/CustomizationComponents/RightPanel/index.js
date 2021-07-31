3c8d09f69418d0d7f1807050d4d6ca087a92c880
Mohit K. Yadav <mohitkyadav@outlook.com>
2021-07-31 08:08:19 +0530

    fix the bounds of drag drop zone

--- src/components/CustomizationComponents/RightPanel/index.js
+++ src/components/CustomizationComponents/RightPanel/index.js
@@ -6,10 +6,24 @@ import './index.scss'
 
 const RightPanel = ({ selectedItem }) => {
   const [pos, setPos] = useState({ x: 0, y: 0 })
+  const heightDragHandle = 40
+  const widthDragHandle = 130
 
   const dragOver = (e) => {
     e.preventDefault()
     e.dataTransfer.dropEffect = 'move'
+  }
+
+  const handleDragEnd = (e) => {
+    const rect = document.getElementById('drop-target').getBoundingClientRect()
+    const posX = e.clientX - rect.left - (widthDragHandle / 2)
+    const posY = e.clientY - rect.top - (heightDragHandle / 2)
+    const x = posX < 0 ? 0 : posX
+    const y = posY < 0 ? 0 : posY
+    setPos({
+      x: Math.min(x, rect.width - widthDragHandle),
+      y: Math.min(y, rect.height - heightDragHandle),
+    })
   }
 
   return (
@@ -30,13 +44,12 @@ const RightPanel = ({ selectedItem }) => {
           style={{
             top: `${pos.y}px`,
             left: `${pos.x}px`,
+            width: `${widthDragHandle}px`,
+            height: `${heightDragHandle}px`,
           }}
           id="drag-target"
           role="presentation"
-          onDragEnd={(e) => {
-            const rect = document.getElementById('drop-target').getBoundingClientRect()
-            setPos({ x: e.clientX - rect.left - 60, y: e.clientY - rect.top - 10 })
-          }}
+          onDragEnd={handleDragEnd}
         />
       </div>
     </div>
